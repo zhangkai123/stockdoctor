8004acb5394edf82bcbf0997fd3e6596ed1d51b2
zhangkai123 <176776005@qq.com>
2013-03-08 21:49:09 +0800

    add the edit and add stock button on nav bar

--- stockdoctor/stockdoctor/SDSelfChooseViewController.m
+++ stockdoctor/stockdoctor/SDSelfChooseViewController.m
@@ -28,8 +28,38 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     titleLabel.text = @"自选股诊断";
-}
+    
+    UIButton *editButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 48.5, 30.5)];
+    [editButton setBackgroundImage:[UIImage imageNamed:@"edit"] forState:UIControlStateNormal];
+    [editButton addTarget:self action:@selector(editStock) forControlEvents:UIControlEventTouchUpInside];
+    [editButton setTitle:[NSString stringWithFormat:@"编辑"] forState:UIControlStateNormal];
+    [editButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica" size:13]];
+    [editButton setTitleColor:[UIColor lightTextColor] forState:UIControlStateNormal];
+    UIBarButtonItem * leftbarButton = [[UIBarButtonItem alloc] initWithCustomView:editButton];
+    [self navigationItem].leftBarButtonItem = leftbarButton;
+    [editButton release];
+    [leftbarButton release];
+    
+    UIButton *addStockButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 66.5, 30.5)];
+    [addStockButton setBackgroundImage:[UIImage imageNamed:@"addstock"] forState:UIControlStateNormal];
+    [addStockButton addTarget:self action:@selector(addStock) forControlEvents:UIControlEventTouchUpInside];
+    [addStockButton setTitle:[NSString stringWithFormat:@"添加股票"] forState:UIControlStateNormal];
+    [addStockButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica" size:13]];
+    [addStockButton setTitleColor:[UIColor lightTextColor] forState:UIControlStateNormal];
+    UIBarButtonItem * rightbarButton = [[UIBarButtonItem alloc] initWithCustomView:addStockButton];
+    [self navigationItem].rightBarButtonItem = rightbarButton;
+    [addStockButton release];
+    [rightbarButton release];
 
+}
+-(void)editStock
+{
+    
+}
+-(void)addStock
+{
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
